549c0cb0250a3e63944336fc1451adef7234364e
Maykon Oliveira <C0635611@vale.com>
2022-08-03 14:31:18 -0300

    Simple Array Sum

---
+++ src/main/kotlin/SimpleArraySum.kt
@@ -0,0 +1,21 @@
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+fun simpleArraySum(ar: Array<Int>): Int {
+    // Write your code here
+    return ar.sum();
+}
+
+fun main(args: Array<String>) {
+    val arCount = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val result = simpleArraySum(ar)
+
+    println(result)
+}
