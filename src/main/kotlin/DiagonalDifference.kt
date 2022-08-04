d82f982cc4457b7416265855da8865ba2c972380
Maykon Oliveira <C0635611@vale.com>
2022-08-04 10:07:11 -0300

    Diagonal Difference

---
+++ src/main/kotlin/DiagonalDifference.kt
@@ -0,0 +1,34 @@
+import kotlin.math.abs
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    // Write your code here
+    var d1 = 0
+    var d2 = 0
+    for (x in arr.indices) {
+        d1 += arr[x][x]
+        d2 += arr[x][arr.size - (x + 1)]
+    }
+
+    return abs(d1 - d2);
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
